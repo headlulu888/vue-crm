da4922be9bca08a968a6853e0f84b03a5596fd23
headlulu888 <headlulu888@gmail.com>
2019-11-17 20:26:19 +0300

    first layout (login && categories)

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,22 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'login',
+    meta: {
+      layout: 'empty'
+    },
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/Categories.vue')
   }
 ]
 

3712ec6643c97c5ed7ed8748f5586c173642855b
headlulu888 <headlulu888@gmail.com>
2019-11-17 20:42:20 +0300

    add more page vue

--- src/router/index.js
+++ src/router/index.js
@@ -25,6 +25,62 @@ const routes = [
       layout: 'main'
     },
     component: () => import('../views/Categories.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: {
+      layout: 'empty'
+    },
+    component: () => import('../views/Register.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/Record.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/Planning.vue')
+  },
+  {
+    path: '/home',
+    name: 'home',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/Home.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/History.vue')
+  },
+  {
+    path: '/detail',
+    name: 'detail',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/Detail.vue')
   }
 ]
 

9ad515f938594b754409e1870c6834922914d52b
headlulu888 <headlulu888@gmail.com>
2019-11-25 23:46:47 +0300

    connect to firebase

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import messagePlugin from '@/utils/message.plugin'
 import dateFilter from '@/filters/date.filters'
 
 import 'materialize-css/dist/js/materialize.min'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
 
 Vue.config.productionTip = false
 
@@ -14,8 +17,25 @@ Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+firebase.initializeApp({
+  apiKey: "AIzaSyCRcqsZw-3K2m7GqCHkyORx5R-6sFYGmZQ",
+  authDomain: "vue-crm-headlulu.firebaseapp.com",
+  databaseURL: "https://vue-crm-headlulu.firebaseio.com",
+  projectId: "vue-crm-headlulu",
+  storageBucket: "vue-crm-headlulu.appspot.com",
+  messagingSenderId: "964998366178",
+  appId: "1:964998366178:web:dff83c4d6d23f30d47afbb",
+  measurementId: "G-37F3BTKJYW"
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})

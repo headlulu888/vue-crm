f8e34c3f45d6cab745f0180417c00a53194dcb29
headlulu888 <headlulu888@gmail.com>
2019-11-26 23:09:36 +0300

    add register func to firebase

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,13 +1,34 @@
 import firebase from 'firebase/app'
 
 export default {
-    actions: {
-        async login({dispatch, commit}, {email, password}) {
-            try {
-                await firebase.auth().signInWithEmailAndPassword(email, password)
-            } catch(e) {
-                 throw e
-            }
-        }
+  actions: {
+    async login({dispatch, commit}, {email, password}) {
+      try {
+          await firebase.auth().signInWithEmailAndPassword(email, password)
+      } catch(e) {
+        console.log(e)
+        throw e
+      }
+    },
+    async register({dispatch}, {email, password, name}) {
+      try {
+          await firebase.auth().createUserWithEmailAndPassword(email, password)
+          const uid = dispatch('getUid')
+          await firebase.database().ref(`/users/${uid}/info`).set({
+            bill: 10000,
+            name: name
+          })
+      } catch(e) {
+        console.log(e)
+        throw e
+      }
+    },
+    async getUid() {
+      const user = firebase.auth().currentUser
+      return user ? user.uid : null
+    },
+    async logout() {
+      await firebase.auth().signOut()
     }
+  }
 }
